4b5cf7a737f187a4a89781c1155294a8d5f0f418
Oh5Yeonju <oh5yeonju@gmail.com>
2023-04-19 23:19:57 +0900

    배열의삭제

---
+++ 001_배열의_삭제.js
@@ -0,0 +1,8 @@
+// 다음 배열에서 400, 500을 삭제하는 code를 입력하세요. 
+var nums = [100, 200, 300, 400, 500]
+// 방법1. slice()
+// console.log(nums.slice(0, 3))
+// 방법2. pop()
+nums.pop()
+nums.pop()
+console.log(nums)
