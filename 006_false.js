60537fee15d0445d2da6ea2cdb4f9afd5f548b15
Oh5Yeonju <oh5yeonju@gmail.com>
2023-04-20 20:45:40 +0900

    006.false

---
+++ 006_false.js
@@ -0,0 +1,20 @@
+// 다음은 자바스크립트의 출력 값은?
+// 정답 false false true true false false false false false true
+Boolean(NaN)
+Boolean("")
+Boolean(1)
+Boolean([])
+Boolean(0)
+Boolean(null) 
+Boolean(undefined) 
+Boolean()
+Boolean( ) 
+Boolean(!NaN) 
+
+// 숫자가 아니면 true, 숫자면 false를 출력
+// false false true
+isNaN(1) 
+isNaN('1')
+isNaN('가나다')
+
+
